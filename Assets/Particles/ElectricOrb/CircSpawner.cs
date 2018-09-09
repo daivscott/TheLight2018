81dbc7577ce66e38c74fdf95027e7db49ccf06ba
daivscott <daivgit@mail.com>
2018-09-09 01:34:45 +0100

    Initial commit

---
+++ Assets/Particles/ElectricOrb/CircSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CircSpawner : NetworkBehaviour {
+
+    public int numObjects;
+    public GameObject prefab;
+
+    void Start()
+    {
+        CmdSpawnLight();
+    }
+
+    //[Command]
+    void CmdSpawnLight()
+    {
+        Vector3 center = transform.position;
+        for (int i = 0; i < numObjects; i++)
+        {
+            Vector3 pos = RandomCircle(center, 0.1f);
+            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center - pos);
+            Instantiate(prefab, pos, rot);
+
+            //NetworkServer.Spawn(prefab);
+        }
+    }
+
+    Vector3 RandomCircle(Vector3 center, float radius)
+    {
+        float ang = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y;
+        pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        return pos;
+    }
+}
---
+++ Assets/Particles/ElectricOrb/Spawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform sphereClone;
+    public Transform sphere;
+
+    private int i = 0;
+    private int iterate = 1;
+    private float displace = 0.5f;
+
+    // Update is called once per frame
+    void Update() {
+
+
+        if (i < 1)
+        {
+            SpawnObject();
+            i++;
+        } else
+        {
+            return;
+        }
+
+
+
+    }
+
+    private void SpawnObject()
+    {
+        if (iterate == 1)
+        {
+            sphere = (Transform)Instantiate(sphereClone, new Vector3(transform.position.x + displace, transform.position.y, transform.position.z), Quaternion.identity);
+            iterate++;
+        } else
+
+            if (iterate == 2)
+            {
+                sphere = (Transform)Instantiate(sphereClone, new Vector3(transform.position.x, transform.position.y, transform.position.z + displace), Quaternion.identity);
+                iterate++;
+            } else
+
+                if (iterate == 3)
+                {
+                    sphere = (Transform)Instantiate(sphereClone, new Vector3(transform.position.x - displace, transform.position.y, transform.position.z), Quaternion.identity);
+                    iterate++;
+                } else 
+
+                    if (iterate == 4)
+                    {
+                        sphere = (Transform)Instantiate(sphereClone, new Vector3(transform.position.x, transform.position.y, transform.position.z - displace), Quaternion.identity);
+                        iterate = 1;
+                    }
+    }
+}
