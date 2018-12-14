7caee4a2435a659b1acf418aed4c7e839834b10d
daivscott <daivgit@mail.com>
2018-12-14 08:39:29 +0000

    Intro Logo and theme audio added

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+
+    private int levelToLoad;
+
+    void Start()
+    {
+        if(SceneManager.GetActiveScene() == SceneManager.GetSceneByName("LoadScreen"))
+        {
+            StartCoroutine(PlayFadeOut());
+        }
+        
+    }
+
+    IEnumerator PlayFadeOut()
+    {
+        yield return new WaitForSeconds(5);
+        FadeToLevel(1);
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
---
+++ Assets/Scripts/GameBackgroundAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBackgroundAudio : MonoBehaviour {
+
+    [SerializeField] AudioSource backingTonesAudio;
+
+    void Start()
+    {
+        //StartCoroutine(PlayBackingAudio());
+        backingTonesAudio.loop = true;
+        backingTonesAudio.Play();
+    }
+
+    //IEnumerator PlayBackingAudio()
+    //{
+    //    yield return new WaitForSeconds(1);
+    //    backingTonesAudio.loop = true;
+    //    backingTonesAudio.Play();
+    //}
+}
